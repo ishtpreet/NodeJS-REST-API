d9c5730310efecdfde75021111f63c47cb9815be
Ishtpreet Singh <500061526@stu.upes.ac.in>
2020-04-21 23:38:05 +0530

    Passport

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
-
+var passport = require('passport');
+var authenticate = require('./authenticate');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var promoRouter = require('./routes/promoRouter');
@@ -32,9 +33,6 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-//app.use(cookieParser('1234655154-45151'));
 app.use(session({
   name: 'session-id',
   secret: '12345-67890-09876-54321',
@@ -42,23 +40,23 @@ app.use(session({
   resave: false,
   store: new FileStore()
 }))
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+//app.use(cookieParser('1234655154-45151'));
+
 function auth (req, res, next) {
   console.log(req.session);
 
-if(!req.session.user) {
+if(!req.user) {
     var err = new Error('You are not authenticated!');
     err.status = 403;
     return next(err);
 }
 else {
-  if (req.session.user === 'authenticated') {
-    next();
-  }
-  else {
-    var err = new Error('You are not authenticated!');
-    err.status = 403;
-    return next(err);
-  }
+  next();
 }
 }
 app.use(auth);
