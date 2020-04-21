1d0061d79581462b849cf50779b83011d44c916f
Ishtpreet Singh <500061526@stu.upes.ac.in>
2020-04-21 22:57:03 +0530

    Express v2

--- app.js
+++ app.js
@@ -32,6 +32,8 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 //app.use(cookieParser('1234655154-45151'));
 app.use(session({
   name: 'session-id',
@@ -42,48 +44,27 @@ app.use(session({
 }))
 function auth (req, res, next) {
   console.log(req.session);
-  if(!req.session.user)
-  {
-    var authHeader = req.headers.authorization;
-    if (!authHeader) {
-        var err = new Error('You are not authenticated!');
-        res.setHeader('WWW-Authenticate', 'Basic');
-        err.status = 401;
-        next(err);
-        return;
-    }
-  
-    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-    var user = auth[0];
-    var pass = auth[1];
-    if (user == 'admin' && pass == 'password') {
-        req.session.user = "admin";
-        next(); // authorized
-    } else {
-        var err = new Error('You are not authenticated!');
-        res.setHeader('WWW-Authenticate', 'Basic');      
-        err.status = 401;
-        next(err);
-    }
+
+if(!req.session.user) {
+    var err = new Error('You are not authenticated!');
+    err.status = 403;
+    return next(err);
+}
+else {
+  if (req.session.user === 'authenticated') {
+    next();
   }
-  else{
-    if(req.session.user === 'admin')
-    {
-      next();
-    }
-    else{
-      var err = new Error('cookie not valid');    
-        err.status = 401;
-        next(err);
-    }
+  else {
+    var err = new Error('You are not authenticated!');
+    err.status = 403;
+    return next(err);
   }
-  
+}
 }
 app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
