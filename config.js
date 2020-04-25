1d3e280a3856733f7e64a20ff90cb84a7ee85337
Ishtpreet Singh <500061526@stu.upes.ac.in>
2020-04-26 02:26:26 +0530

    facebook auth

--- config.js
+++ config.js
@@ -1,4 +1,9 @@
 module.exports = {
     'secretKey':'123-456-789-0321',
-    'mongUrl':'mongodb://localhost:27017/conFusion'
+    'mongUrl':'mongodb://localhost:27017/conFusion',
+    facebook:{
+        clientId: '2711157895780154',
+        clientSecret: 'a0aeb71573a22181b1f9a782648b9677'
+
+    }
 }
